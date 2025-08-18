a514435f217cf1903b0a0e519294e8604ad31ec2
delbyte <blazed0utcode@gmail.com>
2025-08-18 21:44:09 +0530

    Refactor main.ts to enhance animation loop with camera state updates for improved movement control

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { setupControls } from './controls';
 import { setupUI } from './ui';
 import { TerrainGenerator } from './terrain';
 import { updateMovement } from './player';
-
+import { state } from './state';
 import { sampleTerrainParameters } from './sample-terrain-parameters';
 
 const { renderer, scene } = initRenderer(sampleTerrainParameters.skybox, sampleTerrainParameters.lighting);
@@ -19,4 +19,12 @@ scene.add(terrain);
 setupControls(renderer.domElement);
 setupUI(terrainGenerator, scene);
 
-startAnimationLoop(terrainGenerator);
+startAnimationLoop(() => {
+    updateMovement(terrainGenerator);
+    if (state.cameraPosition) {
+        camera.position.copy(state.cameraPosition);
+    }
+    if (state.cameraRotation) {
+        camera.rotation.set(state.cameraRotation.x || 0, state.cameraRotation.y || 0, 0);
+    }
+});
