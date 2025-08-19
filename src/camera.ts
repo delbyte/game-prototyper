6a05854611bac1231dfe968f12d067c3a63259a7
delbyte <blazed0utcode@gmail.com>
2025-08-19 21:24:55 +0530

    Increase camera far plane distance to improve rendering capabilities

--- src/camera.ts
+++ src/camera.ts
@@ -9,7 +9,7 @@ export function initCamera() {
         75,
         window.innerWidth / window.innerHeight,
         0.1,
-        1000
+        10000
     );
     camera.position.copy(state.cameraPosition);
     return camera;
