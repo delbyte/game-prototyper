84bb310f2c14ce16a2668ded2a967bcdac50a2ac
delbyte <blazed0utcode@gmail.com>
2025-08-26 20:31:28 +0530

    Refactor: Add BiomeMaterialType enum for improved biome material categorization

---
+++ src/enums.ts
@@ -0,0 +1,6 @@
+export enum BiomeMaterialType {
+    STANDARD = 'standard',       // Normal terrain (grass, dirt, rock, stone, snow)
+    CRYSTAL = 'crystal',         // Glass, crystal, transparent materials
+    GLOWING = 'glowing',         // Lava, magma, fire, emissive materials
+    WATER = 'water'              // Rivers, lakes, oceans
+}
